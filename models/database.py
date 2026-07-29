d79e88eb015dfeb03124e3d36651c45f183000ab
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2026-07-29 12:42:05 +1000

    Update database password retrieval to default to empty string

--- models/database.py
+++ models/database.py
@@ -9,7 +9,7 @@ def get_connection():
     return mysql.connector.connect(
         host=os.environ.get('DB_HOST', 'localhost'),
         user=os.environ.get('DB_USER', 'root'),
-        password=os.environ.get('DB_PASSWORD', 'bee'),
+        password=os.environ.get('DB_PASSWORD', ''),
         database=os.environ.get('DB_NAME', 'beelektronics_db'),
     )
 
